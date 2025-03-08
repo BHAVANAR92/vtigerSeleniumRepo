3b88e49e6206c55793a82558b33d93d50b9d7429
BHAVANAR92 <39112557+BHAVANAR92@users.noreply.github.com>
2025-03-08 15:27:14 +0530

    Update CampLookUpImg.java

--- AdvancedSelenium_Vtiger/src/main/java/POM_Repo/CampLookUpImg.java
+++ AdvancedSelenium_Vtiger/src/main/java/POM_Repo/CampLookUpImg.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//This class helps to create the caampaigne look up image locater
 public class CampLookUpImg {
 
 	public CampLookUpImg(WebDriver driver) {
